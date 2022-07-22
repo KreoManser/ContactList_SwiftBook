d6797df95008f368d49b7bfb246c4aca9d42cc79
KreoManser <sherba.ru@list.ru>
2022-07-22 18:39:59 +0700

    Initial Commit

---
+++ ContactList_SwiftBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactList_SwiftBook
+//
+//  Created by Сергей Бабич on 22.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
